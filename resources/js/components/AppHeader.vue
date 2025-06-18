e00751cb14fd367de82ef5da98c4e6a07f78d91b
sivakorn-su <101387233+sivakorn-su@users.noreply.github.com>
2025-06-19 02:41:12 +0700

    fix route

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -108,7 +108,7 @@ const rightNavItems: NavItem[] = [
                     </Sheet>
                 </div>
 
-                <Link :href="route('dashboard')" class="flex items-center gap-x-2">
+                <Link :href="route('meetings')" class="flex items-center gap-x-2">
                     <AppLogo />
                 </Link>
 
